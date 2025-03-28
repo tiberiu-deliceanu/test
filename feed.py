6c2a29bd87ba6b283eb2a0c26cb137c85d545ca0
tiberiu-deliceanu <143600300+tiberiu-deliceanu@users.noreply.github.com>
2025-03-28 08:11:36 +0000

    stage files

---
+++ feed.py
@@ -0,0 +1,33 @@
+import yaml
+import xml.etree.ElementTree as xml_tree
+
+with open('feed.yaml', 'r') as file:
+    yaml_data = yaml.safe_load(file)
+
+    rss_element = xml_tree.Element('rss', {'version':'2.0' ,
+    'xmlns:itunes':'http://www.itunes.com/dtds/podcast-1.0.dtd', 
+    'xmlns:content':'http://purl.org/rss/1.0/modules/content/'})
+
+    rss_chanel = xml_tree.SubElement(rss_element, 'channel')
+
+    link_prefix = yaml_data['link'];
+
+    xml_tree.SubElement(rss_chanel,'title').text = yaml_data['title']
+    xml_tree.SubElement(rss_chanel,'format').text = yaml_data['format']
+    xml_tree.SubElement(rss_chanel,'subtitle').text = yaml_data['subtitle']
+    xml_tree.SubElement(rss_chanel,'itunes:author').text = yaml_data['author']
+    xml_tree.SubElement(rss_chanel,'description').text = yaml_data['description']
+    xml_tree.SubElement(rss_chanel,'itunes:image',{'href': link_prefix + yaml_data['image']})
+    xml_tree.SubElement(rss_chanel,'link').text = link_prefix
+
+    xml_tree.SubElement(rss_chanel,'category', {'text': yaml_data['category']})
+
+    for item in yaml_data['item']:
+        item_element = xml_tree.SubElement(rss_element, 'item')          
+        xml_tree.SubElement(item_element,'title').text = item['title']
+        xml_tree.SubElement(item_element,'description').text = item['description']
+        xml_tree.SubElement(item_element,'duration').text = item['duration']
+
+
+    output_tree = xml_tree.ElementTree(rss_element)
+    output_tree.write('podcast.xml',encoding='UTF-8',xml_declaration=True)
